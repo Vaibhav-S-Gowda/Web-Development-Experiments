927c6e5baa0803ed3123265f4ab0b16967c66d49
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-03-26 21:39:00 +0530

    initial commit

---
+++ JavaScript/prompt.js
@@ -0,0 +1,7 @@
+var a = Number(prompt("What is the value of 'a'? \n"))
+var b = Number(prompt("What is the value of 'b'? \n"))
+var c = Number(prompt("What is the value of 'c'? \n"))
+
+var add = a + b + c;
+
+document.write("Addition of three number is = " + add);
