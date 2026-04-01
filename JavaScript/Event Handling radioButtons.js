770cd91de759aef45b390f59e7aba6861a740195
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-04-01 09:10:14 +0530

    initial commit

---
+++ JavaScript/Event Handling radioButtons.js
@@ -0,0 +1,19 @@
+function laptopChoice(key) {
+    switch (key) {
+        case 1:
+            alert("High performance laptop with powerful GPU for gaming");
+            break;
+        case 2:
+            alert("Affordable laptop suitable for students and learning professionals");
+            break;
+        case 3:
+            alert("Reliable laptop with security features for office work");
+            break;
+        case 4:
+            alert("Laptops for students");
+            break;
+        default:
+            alert("Invalid Choice");
+            break;
+    }
+}
