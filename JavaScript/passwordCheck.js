74e434624efcd032ed3961530f025dc750e31584
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-04-01 09:10:54 +0530

    initial commit

---
+++ JavaScript/passwordCheck.js
@@ -0,0 +1,16 @@
+function chkPasswords() {
+    var init = document.getElementById("initial");
+    var sec = document.getElementById("second");
+
+    if (init.value == "") {
+        alert("You did not enter a password\nPlease enter one now");
+        init.focus();
+        return false;
+    }
+
+    if (init.value != sec.value) {
+        alert("Please enter the correct password");
+        init.focus();
+        return false;
+    }
+}
