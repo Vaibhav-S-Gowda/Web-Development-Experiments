3d25106f10f1b1bcd1b1fe677059edaf41b46fad
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-04-07 19:28:17 +0530

    initial commit

---
+++ Node/Octal and Binary literal.js
@@ -0,0 +1,26 @@
+let a = 051;
+console.log(a);
+
+let b = 058;
+console.log(b);
+
+let c = 0o51;
+console.log(c);
+
+let d = parseInt('111', 2)
+console.log(d);
+
+let e = 0b111;
+console.log(e);
+
+let f = 0x10;
+console.log(f);
+
+let text = `This is a template literal`;
+console.log(text)
+
+let g = 0x1A;
+console.log(g);
+
+let h = 0b1011;
+console.log(h);
