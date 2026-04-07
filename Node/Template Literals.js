9896dec2615af875cc01cd15b01d805da912ca24
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-04-07 19:27:59 +0530

    initial commit

---
+++ Node/Template Literals.js
@@ -0,0 +1,13 @@
+let name = "Shannu";
+let msg = `Hello ${name}`;
+console.log(msg);
+
+let a = 5, b = 3;
+console.log(`Sum is ${a + b}`);
+
+// Multi-line string
+const text = `This is line1
+    and line2
+        then line3`
+console.log(text)
+
