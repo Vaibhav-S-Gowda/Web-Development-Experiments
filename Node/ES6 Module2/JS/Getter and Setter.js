903013cf4e1ac4d7fc21c15ac8d917a63e140501
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-04-09 10:33:15 +0530

    initial commit

---
+++ Node/ES6 Module2/JS/Getter and Setter.js
@@ -0,0 +1,14 @@
+class Student {
+    constructor(name) {
+        this.setName(name);
+    }
+    getName() {
+        return this.name;
+    }
+    setName(newName) {
+        newName = newName.trim();
+        if (newName === '') {
+            
+        }
+    }
+}
