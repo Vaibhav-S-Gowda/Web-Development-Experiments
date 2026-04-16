465b7e544926a9fa6c4097d29ad2c395dd5e2576
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-04-17 00:12:27 +0530

    initial commit

---
+++ React/create-react-app/create-react-app/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
+
+// const x = 15;
+
+// const myElement = <h1>{(x) < 10 ? "Hello bruh" : "Good Night"}</h1>;
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(myElement);
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
