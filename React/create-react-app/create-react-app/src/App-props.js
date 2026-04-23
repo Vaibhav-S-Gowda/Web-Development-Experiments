57ff66daea27b7169607890dcde892451ab30e89
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-04-23 19:20:55 +0530

    modified

--- React/create-react-app/create-react-app/src/App-props.js
+++ React/create-react-app/create-react-app/src/App-props.js
@@ -1,21 +1,22 @@
 import React from "react";
-import Book from './components/Book';
+// import Book from './components/Book';
+import BookClass from './components/BookClass';
 
 function App() {
     return (
         <div style={{border: "1px solid gray", margin: "10px", paddding: "10px", color: "orange"}}>
             <h1>Book Store</h1>
             {/*<BookClass*/}
-            <Book title = "React Basics"
+            <BookClass title = "React Basics"
             author = "Dan Abramov"
             price = {499}/>
-            <Book title = "Algorithms Analysis and Design"
+            <BookClass title = "Algorithms Analysis and Design"
             author = "Shanmukha Ishwar Bhat"
             price = {999}/>
-            <Book title = "Data Communications and Network"
+            <BookClass title = "Data Communications and Network"
             author = "Kalpana "
             price = {699}/>
-            <Book title = "Scalable Data Processing"
+            <BookClass title = "Scalable Data Processing"
             author = "Niteesh Kumar"
             price = {899}/>
         </div>
