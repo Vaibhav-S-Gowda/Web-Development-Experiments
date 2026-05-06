34ff95433c6a8c63e88cfd05c9f0dabf49bfbc5a
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-05-06 20:15:23 +0530

    initial commit

---
+++ React/create-react-app/create-react-app/src/components/App-props.js
@@ -0,0 +1,26 @@
+import React from "react";
+// import Book from './components/Book';
+import BookClass from './BookClass';
+
+function App() {
+    return (
+        <div style={{border: "1px solid gray", margin: "10px", paddding: "10px", color: "orange"}}>
+            <h1>Book Store</h1>
+            {/*<BookClass*/}
+            <BookClass title = "React Basics"
+            author = "Dan Abramov"
+            price = {499}/>
+            <BookClass title = "Algorithms Analysis and Design"
+            author = "Shanmukha Ishwar Bhat"
+            price = {999}/>
+            <BookClass title = "Data Communications and Network"
+            author = "Kalpana "
+            price = {699}/>
+            <BookClass title = "Scalable Data Processing"
+            author = "Niteesh Kumar"
+            price = {899}/>
+        </div>
+    );
+}
+
+export default App;
