6852cf486a3fb0f95ac33dfd6e1f0f45504da548
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-05-07 22:29:43 +0530

    initial commit

---
+++ React/create-react-app/create-react-app/src/components/AppDynamic.js
@@ -0,0 +1,33 @@
+import BookTable from "./BookTable";
+
+function App() {
+    const books = [
+        {
+            isbn: "978-0-262-13472-9",
+            title: "React Basics",
+            author: "Dan Abramov",
+            price: 499,
+            published: new Date('2004-04-04'),
+            publisherName: "Springer"
+        },
+        {
+            isbn: "978-1-638564-345-9",
+            title: "Javascript Essentials",
+            author: "Brendan",
+            price: 299,
+            published: new Date('2004-04-04'),
+            publisherName: "Springer"
+        }
+    ];
+
+    return (
+        <div className="App">
+            <header className="App-header">
+            <h1>Book Store</h1>
+            </header>
+            <BookTable books={books} />
+        </div>
+    );
+}
+
+export default App;
