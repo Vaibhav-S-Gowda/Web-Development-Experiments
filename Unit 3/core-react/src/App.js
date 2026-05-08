b2823ea52326aa0da33baf13d9e1424ea8a4b8a6
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-05-08 22:58:55 +0530

    updated file

--- Unit 3/core-react/src/App.js
+++ Unit 3/core-react/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import './App_Country.css';
+import React, { Component } from 'react';
+import CountryTable from './components/Country_Table';
+import CountryAdd from './components/Country_Add';
+import CountryFilter from './components/Country_Filter';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            countries: [
+                { name: "India", capital: "New Delhi" },
+                { name: "USA", capital: "Washington D.C." },
+                { name: "Japan", capital: "Tokyo" }
+            ],
+            search: ''
+        };
+    }
+
+    addCountry = (newCountry) => {
+        this.setState({
+            countries: [...this.state.countries, newCountry]
+        });
+    };
+
+    handleSearch = (value) => {
+        this.setState({ search: value });
+    };
+
+    render() {
+        // 🔥 FILTER LOGIC HERE
+        const filteredCountries = this.state.countries.filter(c =>
+            c.name.toLowerCase().includes(this.state.search.toLowerCase())
+        );
+
+        return (
+            <div className="App">
+                <h1>Country Tracker</h1>
+
+                <CountryFilter handleSearch={this.handleSearch} />
+                <hr />
+
+                <CountryTable countries={filteredCountries} />
+                <hr />
+
+                <CountryAdd addCountry={this.addCountry} />
+            </div>
+        );
+    }
 }
-
 export default App;
