b31e2224286763b7574d326a90fdb964730902a1
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-05-14 10:34:28 +0530

    minor changes

--- Unit 3/core-react/src/index.js
+++ Unit 3/core-react/src/index.js
@@ -5,7 +5,8 @@ import './index.css'
 // import App from './Components/App_State';
 // import App from './App_Country';
 // import App from './App_Event';
-import App from './App-stateless'
+// import App from './App-stateless';
+import App from './App_Child_to_Parent';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
