4c383585509878d15a09753466cf532fb0c3463f
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-05-14 10:34:45 +0530

    initial commit

---
+++ Unit 3/core-react/src/App_Child_to_Parent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Parent from "./functional-components/parentfn";
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header"></header>
+            <Parent/>
+        </div>
+    );
+}
+
+export default App;
