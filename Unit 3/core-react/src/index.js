1fffa5d1d992d7348fbbb50fcebb4938a39f3384
Vaibhav-S-Gowda <vaibhav.satheesh03@gmail.com>
2026-05-14 10:01:21 +0530

    made some minor changes

--- Unit 3/core-react/src/index.js
+++ Unit 3/core-react/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css'
 // import App from './App';
 // import App from './Components/App_State';
 // import App from './App_Country';
-import App from './App_Event';
+// import App from './App_Event';
+import App from './App-stateless'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
